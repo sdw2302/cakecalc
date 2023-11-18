0f0ebbf202e015084fb4b4e922bfc41bfdab1ac6
sdw2302 <shadow2302ezg@gmail.com>
2023-11-18 23:21:31 +0100

    basic add product menu

--- src/main/java/martin/PrimaryController.java
+++ src/main/java/martin/PrimaryController.java
@@ -1,12 +1,14 @@
 package martin;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
+    TextField productName, boughtQuantity, usedQuantity, priceBought, priceUp;
+
+    @FXML
+    RadioButton priceRadio1, priceRadio2;
 }
